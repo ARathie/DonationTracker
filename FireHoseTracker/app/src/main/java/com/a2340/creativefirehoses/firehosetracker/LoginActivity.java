5172f5ba2a6e571484951e4b1dd219f4b77449ea
Andrew Kristanto <andrewkristanto555@gmail.com>
2018-09-24 22:27:42 -0400

    test login

--- FireHoseTracker/app/src/main/java/com/a2340/creativefirehoses/firehosetracker/LoginActivity.java
+++ FireHoseTracker/app/src/main/java/com/a2340/creativefirehoses/firehosetracker/LoginActivity.java
@@ -3,6 +3,7 @@ package com.a2340.creativefirehoses.firehosetracker;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -186,7 +187,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // perform the user login attempt.
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+            mAuthTask.execute();
         }
     }
 
@@ -332,8 +333,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = null;
             showProgress(false);
 
-            if (success) {
-                finish();
+            if (mEmail.equals("test@test.com") && mPassword.equals("testtest")) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
