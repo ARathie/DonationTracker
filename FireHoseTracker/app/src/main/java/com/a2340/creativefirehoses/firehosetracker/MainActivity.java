43e1685792613226efdf7b3b09836170bd3d07fa
ARathie <ashwin.rathie@gmail.com>
2018-09-27 15:52:43 -0400

    Add functoinality to logout button

--- FireHoseTracker/app/src/main/java/com/a2340/creativefirehoses/firehosetracker/MainActivity.java
+++ FireHoseTracker/app/src/main/java/com/a2340/creativefirehoses/firehosetracker/MainActivity.java
@@ -2,12 +2,30 @@ package com.a2340.creativefirehoses.firehosetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonLogOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonLogOut = (Button) findViewById(R.id.buttonLogOut);
+        buttonLogOut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
