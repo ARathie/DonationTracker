a84d383cd7c453f8c09f36c71cda1c326920e8b6
Andrew Kristanto <andrewkristanto555@gmail.com>
2018-10-18 17:27:36 -0400

    bug fixing list

--- FireHoseTracker/app/src/main/java/com/a2340/creativefirehoses/firehosetracker/LocationModel.java
+++ FireHoseTracker/app/src/main/java/com/a2340/creativefirehoses/firehosetracker/LocationModel.java
@@ -9,17 +9,24 @@ public class LocationModel {
     public static final LocationModel INSTANCE = new LocationModel();
 
     private List<LocationItem> locations;
+    private List<String> locationNames;
 
     private LocationModel() {
         locations = new ArrayList<>();
+        locationNames = new ArrayList<>();
     }
 
     public void addItem(LocationItem item) {
         locations.add(item);
+        locationNames.add(item.getLocationName());
     }
 
     public List<LocationItem> getLocations() {
         return locations;
+    }
+
+    public List<String> getLocationNames() {
+        return locationNames;
     }
 
     public LocationItem findItemByName(String name) {
---
+++ FireHoseTracker/app/src/main/java/com/a2340/creativefirehoses/firehosetracker/ViewLocationsActivity.java
@@ -0,0 +1,68 @@
+package com.a2340.creativefirehoses.firehosetracker;
+
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.widget.ArrayAdapter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class ViewLocationsActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_locations);
+
+        readSDFile();
+        LocationModel model = LocationModel.INSTANCE;
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(),
+                android.R.layout.simple_list_item_1, model.getLocationNames());
+        getListView().setAdapter(adapter);
+
+    }
+
+    public static final int LOCATION_NAME = 1;
+    public static final int LATITUDE = 2;
+    public static final int LONGITUDE = 3;
+    public static final int STREET_ADDRESS = 4;
+    public static final int CITY = 5;
+    public static final int STATE = 6;
+    public static final int ZIP = 7;
+    public static final int TYPE = 8;
+    public static final int PHONE_NUM = 9;
+    public static final int WEBSITE = 10;
+    /**
+     * Open the sample.csv file in the /res/raw directory
+     * Line Entry format:
+     *   [0] - name
+     *
+     */
+    private void readSDFile() {
+        LocationModel model = LocationModel.INSTANCE;
+
+        try {
+            //Open a stream on the raw file
+            InputStream is = getResources().openRawResource(R.raw.locationdata);
+            //From here we probably should call a model method and pass the InputStream
+            //Wrap it in a BufferedReader so that we get the readLine() method
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+
+            String line;
+            br.readLine(); //get rid of header line
+            while ((line = br.readLine()) != null) {
+                String[] tokens = line.split(",");
+                model.addItem(new LocationItem(tokens[LOCATION_NAME], tokens[LATITUDE],
+                        tokens[LONGITUDE], tokens[STREET_ADDRESS], tokens[CITY], tokens[STATE],
+                        tokens[ZIP], tokens[TYPE], tokens[PHONE_NUM], tokens[WEBSITE]));
+            }
+            br.close();
+        } catch (IOException e) {
+
+        }
+
+    }
+}
