992ab69f33421f3d26143efd1f5b5f236e8ea2fa
Sohan Choudhury <sohanyo@gmail.com>
2018-11-01 22:43:11 -0400

    search activity logic

--- FireHoseTracker/app/src/main/java/com/a2340/creativefirehoses/firehosetracker/SearchActivity.java
+++ FireHoseTracker/app/src/main/java/com/a2340/creativefirehoses/firehosetracker/SearchActivity.java
@@ -44,14 +44,77 @@ import static android.Manifest.permission.READ_CONTACTS;
  */
 public class SearchActivity extends AppCompatActivity {
 
+
+    private EditText itemField;
+    private Spinner category;
+    private EditText locField;
+    private Model model = Model.getInstance();
+    private ArrayList<Item> inv;
+    private Location loc;
+    private ArrayList<String> cats;
+
     private int locationPosition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_view);
 
+        itemField = findViewById(R.id.editText2);
+        category = findViewById(R.id.LocSpinner);
+        locField = findViewById(R.id.editText);
+
+        cats = new ArrayList<>(Arrays.asList(Item.categories));
+        cats.add(0, "");
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, cats);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        category.setAdapter(adapter);
+
+        if (getIntent().hasExtra("Act") && getIntent().getStringExtra("Act").equals("LocationRecyclerActivity")) {
+            loc = getIntent().getParcelableExtra("Location");
+            itemField.setText("");
+            locField.setText(loc.getName());
+        } else {
+            itemField.setText("");
+            locField.setText("");
+            category.setSelection(0);
+        }
 
     }
+
+    public void onBackPressed(View v) {
+        Intent intent = new Intent(this, AppLandingActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSearchPressed(View v) {
+        Intent intent = new Intent(this, InventoryRecyclerActivity.class);
+        inv = model.itemSearch(itemField.getText().toString(), category.getSelectedItem().toString(), loc);
+        if (inv.size() == 0) {
+            Snackbar failed = Snackbar.make(v, "No matches!", Snackbar.LENGTH_SHORT);
+            failed.show();
+        } else {
+            intent.putParcelableArrayListExtra("Inventory", inv);
+            intent.putExtra("Act", "SearchActivity");
+            startActivity(intent);
+        }
+    }
+
+    public void onSearchLocPressed(View v) {
+        Intent intent = new Intent(this, LocationRecyclerActivity.class);
+        ArrayList<Location> locs = model.locSearch(locField.getText().toString());
+        if (locs.size() == 0) {
+            Snackbar failed = Snackbar.make(v, "No matches!", Snackbar.LENGTH_SHORT);
+            failed.show();
+        } else {
+            intent.putParcelableArrayListExtra("Locations",  locs);
+            intent.putExtra("Act", "SearchActivity");
+            startActivity(intent);
+        }
+    }
+
 }
 
